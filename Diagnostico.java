4296e20484d47caf0a885fe5424c9629e66a5e92
Rebatt <38532831+Rebatt@users.noreply.github.com>
2018-04-19 16:24:23 +0200

    Update Diagnostico.java

--- Diagnostico.java
+++ Diagnostico.java
@@ -1,1 +1,156 @@
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.sql.*;
 
+public class Diagnostico {
+
+	private final String DATAFILE = "data/disease_data.data";
+	Connection conn;
+	Statement st;
+	private void showMenu() {
+
+		int option = -1;
+		do {
+			System.out.println("Bienvenido a sistema de diagn�stico\n");
+			System.out.println("Selecciona una opci�n:\n");
+			System.out.println("\t1. Creaci�n de base de datos y carga de datos.");
+			System.out.println("\t2. Realizar diagn�stico.");
+			System.out.println("\t3. Listar s�ntomas de una enfermedad.");
+			System.out.println("\t4. Listar enfermedades y sus c�digos asociados.");
+			System.out.println("\t5. Listar s�ntomas existentes en la BD y su tipo sem�ntico.");
+			System.out.println("\t6. Mostrar estad�sticas de la base de datos.");
+			System.out.println("\t7. Salir.");
+			try {
+				option = readInt();
+				switch (option) {
+				case 1:
+					crearBD();
+					break;
+				case 2:
+					realizarDiagnostico();
+					break;
+				case 3:
+					listarSintomasEnfermedad();
+					break;
+				case 4:
+					listarEnfermedadesYCodigosAsociados();
+					break;
+				case 5:
+					listarSintomasYTiposSemanticos();
+					break;
+				case 6:
+					mostrarEstadisticasBD();
+					break;
+				case 7:
+					exit();
+					break;
+				}
+			} catch (Exception e) {
+				System.err.println("Opci�n introducida no v�lida!");
+			}
+		} while (option != 7);
+		exit();
+	}
+
+	private void exit() {
+		System.out.println("Saliendo.. �hasta otra!");
+		System.exit(0);
+	}
+	
+	private void conectar() {
+		try {
+			conn = DriverManager.getConnection("com.mysql.jdbc.Driver");
+			st = conn.createStatement();
+		}catch(SQLException ex) {
+			System.err.println(ex.getMessage());
+		}
+		
+	}
+
+	private void crearBD() {
+		try {
+			conectar();
+			String nombre = "DIAGNOSTICO";
+			st.executeUpdate("CREATE DATABASE " + nombre);
+		}catch(SQLException ex) {
+			System.err.println(ex.getMessage());
+		}
+		
+	}
+
+	private void realizarDiagnostico() {
+		// implementar
+	}
+
+	private void listarSintomasEnfermedad() {
+		// implementar
+	}
+
+	private void listarEnfermedadesYCodigosAsociados() {
+		// implementar
+	}
+
+	private void listarSintomasYTiposSemanticos() {
+		// implementar
+	}
+
+	private void mostrarEstadisticasBD() {
+		// implementar
+	}
+
+	/**
+	 * M�todo para leer n�meros enteros de teclado.
+	 * 
+	 * @return Devuelve el n�mero le�do.
+	 * @throws Exception
+	 *             Puede lanzar excepci�n.
+	 */
+	private int readInt() throws Exception {
+		try {
+			System.out.print("> ");
+			return Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+		} catch (Exception e) {
+			throw new Exception("Not number");
+		}
+	}
+
+	/**
+	 * M�todo para leer cadenas de teclado.
+	 * 
+	 * @return Devuelve la cadena le�da.
+	 * @throws Exception
+	 *             Puede lanzar excepci�n.
+	 */
+	private String readString() throws Exception {
+		try {
+			System.out.print("> ");
+			return new BufferedReader(new InputStreamReader(System.in)).readLine();
+		} catch (Exception e) {
+			throw new Exception("Error reading line");
+		}
+	}
+
+	/**
+	 * M�todo para leer el fichero que contiene los datos.
+	 * 
+	 * @return Devuelve una lista de String con el contenido.
+	 * @throws Exception
+	 *             Puede lanzar excepci�n.
+	 */
+	private LinkedList<String> readData() throws Exception {
+		LinkedList<String> data = new LinkedList<String>();
+		BufferedReader bL = new BufferedReader(new FileReader(DATAFILE));
+		while (bL.ready()) {
+			data.add(bL.readLine());
+		}
+		bL.close();
+		return data;
+	}
+
+	public static void main(String args[]) {
+		new Diagnostico().showMenu();
+	}
+}
+ 
