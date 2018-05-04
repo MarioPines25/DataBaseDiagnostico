fec0c09b47103d376f134d30474956258435f2e1
MarioPines25 <38532882+MarioPines25@users.noreply.github.com>
2018-05-04 19:51:29 +0200

    Update Diagnostico.java

--- Diagnostico.java
+++ Diagnostico.java
@@ -82,6 +82,123 @@ public class Diagnostico {
 		try {
 			conectar();
 			st.executeUpdate("CREATE SCHEMA `diagnostico` ; ");
+			
+			
+			st=conn.createStatement();
+
+
+			//CREACION DE TABLAS
+
+			// Tabla disease:
+			String disease = "CREATE TABLE `diagnostico`.`disease`("
+					+ "`disease_id` INT NOT NULL," 
+					+ "`name` VARCHAR(255) NOT NULL," 
+					+ "PRIMARY KEY (`disease_id`))";
+			ResultSet rs = st.executeQuery(disease);
+			int numero = rs.getInt(2);
+
+			// Tabla symptom:
+			String symptom ="CREATE TABLE `diagnostico`.`symptom` ("+
+					"`cui` VARCHAR(25) NOT NULL," +
+					"`name` VARCHAR(255) NOT NULL,"+
+					"PRIMARY KEY (`cui`));";
+
+			ResultSet rs1 = st.executeQuery(symptom);
+
+			// Tabla source
+			String source = "CREATE TABLE `diagnostico`.`source` ( "+
+					"`source_id` INT NOT NULL," +
+					"`name` VARCHAR(255) NOT NULL," + 
+					"PRIMARY KEY (`source_id`));";
+			ResultSet rs2 = st.executeQuery(source);
+
+			// Tabla semantic_type
+			String semantic_type = "CREATE TABLE `diagnostico`.`semantic_type` (" +
+					"`semantic_type_id` INT NOT NULL," +
+					"`cui` VARCHAR(45) NOT NULL," +
+					"PRIMARY KEY (`semantic_type_id`));";
+			ResultSet rs3 = st.executeQuery(semantic_type);
+
+			// Tabla symptom_semantic_type
+			String symptom_semantic_type = "CREATE TABLE `diagnostico`.`symptom_semantic_type` (" +
+					"`cui_sst` VARCHAR(25) NOT NULL," +
+					"`semantic_type_id_sst` INT NOT NULL," +
+					"INDEX (`cui_sst` ASC)," +
+					"INDEX `semantic_type_id_sst_idx` (`semantic_type_id_sst` ASC)," +
+					"CONSTRAINT `cui_sst`"+
+					"FOREIGN KEY (`cui_sst`)"+
+					"REFERENCES `diagnostico`.`symptom` (`cui`)"+
+					"ON DELETE NO ACTION"+
+					"ON UPDATE NO ACTION,"+
+					"CONSTRAINT `semantic_type_id_sst`"+
+					"FOREIGN KEY (`semantic_type_id_sst`)"+
+					"REFERENCES `diagnostico`.`semantic_type` (`semantic_type_id`)"+
+					"ON DELETE NO ACTION"+
+					" ON UPDATE NO ACTION);";
+			ResultSet rs4 = st.executeQuery(symptom_semantic_type);
+
+			// Tabla disease_symptom
+			String disease_symptom = "CREATE TABLE `diagnostico`.`disease_symptom` (" +
+					"`disease_id_ds` INT NOT NULL," +
+					"`cui_ds` VARCHAR(45) NOT NULL," +
+					"INDEX `disease_id_ds_idx` (`disease_id_ds` ASC)," +
+					"INDEX `cui_ds_idx` (`cui_ds` ASC)," +
+					"CONSTRAINT `disease_id_ds`" +
+					"FOREIGN KEY (`disease_id_ds`)" +
+					"REFERENCES `diagnostico`.`disease` (`disease_id`)"+
+					"ON DELETE NO ACTION"+
+					"ON UPDATE NO ACTION,"+
+					"CONSTRAINT `cui_ds`" +
+					" FOREIGN KEY (`cui_ds`)" +
+					"REFERENCES `diagnostico`.`symptom` (`cui`)" +
+					"ON DELETE NO ACTION" +
+					" ON UPDATE NO ACTION);";
+			ResultSet rs5 = st.executeQuery(disease_symptom);
+
+			// Tabla code
+			String code="CREATE TABLE `diagnostico`.`code` ("+
+					"`code` VARCHAR(255) NOT NULL,"+
+					"`source_id_c` INT NOT NULL," +
+					"PRIMARY KEY (`code`),"+
+					"INDEX `source_id_c_idx` (`source_id_c` ASC)," +
+					"CONSTRAINT `source_id_c`" +
+					"FOREIGN KEY (`source_id_c`)" +
+					"REFERENCES `diagnostico`.`source` (`source_id`)" +
+					"ON DELETE NO ACTION"+
+					"ON UPDATE NO ACTION);";
+
+			ResultSet rs6= st.executeQuery(code);
+
+			//Tabla disease_has_code
+			String disease_has_code = "CREATE TABLE `diagnostico`.`disease_has_code` (" +
+					"`disease_id_dhc` INT NULL," +
+					"`code_dhc` VARCHAR(255) NULL," +
+					"`source_id_dhc` INT NULL," +
+					"INDEX `disease_id_dhc_idx` (`disease_id_dhc` ASC),"+
+					"INDEX `code_dhc_idx` (`code_dhc` ASC)," +
+					"INDEX `source_id_dhc_idx` (`source_id_dhc` ASC),"+
+					"CONSTRAINT `disease_id_dhc`" +
+					"FOREIGN KEY (`disease_id_dhc`)"+
+					"REFERENCES `diagnostico`.`disease` (`disease_id`)" +
+					"ON DELETE NO ACTION"+
+					"ON UPDATE NO ACTION," +
+					"CONSTRAINT `code_dhc`" +
+					"FOREIGN KEY (`code_dhc`)"+
+					"REFERENCES `diagnostico`.`code` (`code`)"+
+					"ON DELETE NO ACTION" +
+					"ON UPDATE NO ACTION,"+
+					"CONSTRAINT `source_id_dhc`"+
+					"FOREIGN KEY (`source_id_dhc`)" +
+					"REFERENCES `diagnostico`.`source` (`source_id`)"+
+					"ON DELETE NO ACTION"+
+					"ON UPDATE NO ACTION);";
+
+			ResultSet rs7 = st.executeQuery(disease_has_code);
+
+
+			//Obtencion de los datos a traves del archivo DATA
+			
+			
 			LinkedList<String> list = readData();
 			String []enfermedades;
 			String []codVoc;
@@ -95,11 +212,12 @@ public class Diagnostico {
 				enfermedades=enfSint[0].split(":");
 				codVoc=enfermedades[1].split(";");
 
-				for(int j=0;i<codVoc.length;j++){
+				for(int j=0;j<codVoc.length;j++){
 					//conseguimos codigos y vocabularios
-					codigo=codVoc[i].split("@");
+					codigo=codVoc[j].split("@");
 				}
 				//FIN PARTE IZQUIERDA ARBOL
+
 				//COMIENZO PARTE DERECHA ARBOL
 				String [] sintomas;
 				String [] elementos;
@@ -107,18 +225,36 @@ public class Diagnostico {
 				for (int j=0; i<sintomas.length;j++){
 					//System.out.println(sintomas[i]);
 					elementos= sintomas[j].split(":");
-					for (int z=0; j< elementos.length;z++){
-						//datos= elementos[j].split(":");
-						System.out.println(elementos[z]); 
-					}
+					
 				}
+
+				/*
+				 * A la salida de los bucles, los datos se distribuyen:
+				 * 		enfermedades = 	contiene el nombre de todas las enfermedades (0-10 --> 11 enfermedades)
+				 * 		codVoc = 		tiene el codigo y el vocabulario de cada enfermedad.
+				 * 		codigo = 		posiciones pares: codigo
+				 * 				 		posiciones impares: vocabulario
+				 * 		elementos =		(diferencia entre elementos = 3)
+				 * 						contiene el sintoma, su codigo y tipo semantico. 
+				 * 
+				 * Cada iteración ira haciendo esta separaciones por lo que debemos introducir en cada una
+				 * de las tablas los datos necesarios de cada array.
+				 */
+
+
+
+
 			}
 
 
+			
 		}catch(SQLException ex) {
 			System.err.println(ex.getMessage());
 		}
 		conn.close();
+
+	}
+
 
 	}
 	private int numMaxSyntom() {
